f173390d8096d67cc0a69d4e12599e0c64118173
AaronthiccThai <howaaronic1@gmail.com>
2026-01-10 23:01:33 +1100

    Added an exp bar to play around with

--- app/src/main/java/com/example/cordyx/MainActivity.kt
+++ app/src/main/java/com/example/cordyx/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import com.example.cordyx.ui.components.XpViewModel
 import com.example.cordyx.ui.theme.CordyxTheme
 import com.example.cordyx.ui.screens.TitleScreen
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CordyxTheme {
-                TitleScreen()
+                val xpViewModel: XpViewModel = XpViewModel()
+                TitleScreen(xpViewModel)
+
             }
         }
     }
--- app/src/main/java/com/example/cordyx/ui/screens/TitleScreen.kt
+++ app/src/main/java/com/example/cordyx/ui/screens/TitleScreen.kt
@@ -2,22 +2,33 @@ package com.example.cordyx.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.cordyx.R
+import com.example.cordyx.ui.components.XpProgressBar
+import com.example.cordyx.ui.components.XpViewModel
 
 
 @Composable
-fun TitleScreen() {
+fun TitleScreen(xpViewModel: XpViewModel = viewModel()) {
+    var count by remember { mutableIntStateOf(0) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -36,5 +47,40 @@ fun TitleScreen() {
             text = stringResource(id = R.string.tagline),
             style = MaterialTheme.typography.bodyLarge
         )
+        Text("Count: $count")
+
+
+        Row(
+            modifier = Modifier
+                .padding(32.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+        }
+        XpProgressBar(
+            currentXp = xpViewModel.questXp.currentXp,
+            xpToNextLevel = xpViewModel.questXp.xpToNextLevel,
+            level = xpViewModel.level,
+            onAddXp = { xpViewModel.gainQuestXp(10) }
+
+
+        )
+        XpProgressBar(
+            currentXp = xpViewModel.dailyXp.currentXp,
+            xpToNextLevel = xpViewModel.dailyXp.xpToNextLevel,
+            level = xpViewModel.level,
+            onAddXp = { xpViewModel.gainDailyXp(10) }
+        )
+
     }
+
+}
+
+
+@Composable
+fun PrimaryButton(text: String, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text)
+    }
+
 }
